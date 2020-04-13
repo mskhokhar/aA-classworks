68c151fcaedd5274c23fd86ad0d76415630b171d
Manraj <manrajkhokhar1995@gmail.com>
2020-04-12 23:30:16 -0700

    add selection sort

--- w15d2/selection_sort_project/lib/selection_sort.js
+++ w15d2/selection_sort_project/lib/selection_sort.js
@@ -5,7 +5,16 @@ function swap(arr, index1, index2) {
 }
 
 function selectionSort(arr) {
-    
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex=j;
+            }
+        }
+        swap(arr,i,minIndex);
+    }
+    return arr;
 }
 
 module.exports = {

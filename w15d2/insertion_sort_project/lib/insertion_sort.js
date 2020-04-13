e4183d80d690de4ac19a8725e783638df1ef9214
Manraj <manrajkhokhar1995@gmail.com>
2020-04-12 23:46:16 -0700

    solve insertion sort

--- w15d2/insertion_sort_project/lib/insertion_sort.js
+++ w15d2/insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,12 @@
-function insertionSort(arr) {
-
+function insertionSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        let currElement = array[i];
+        for (var j = i-1; j>=0 && currElement<array[j]; j--) {
+            array[j+1] = array[j]
+        }
+        array[j+1] = currElement;
+    }
+    return array;
 }
 
 module.exports = {

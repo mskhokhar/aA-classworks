6ba30f06820d30d9a0429a178586cac6471d2038
Manraj <manrajkhokhar1995@gmail.com>
2020-04-12 18:23:43 -0700

    fix npm issues and solve swap method

--- w15d2/selection_sort_project/lib/selection_sort.js
+++ w15d2/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,11 @@
 function swap(arr, index1, index2) {
-
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
 }
 
 function selectionSort(arr) {
-
+    
 }
 
 module.exports = {

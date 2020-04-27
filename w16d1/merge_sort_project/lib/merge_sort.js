9f8c46775e093f8960f3c0634ed0dfa8e21980d1
Manraj <manrajkhokhar1995@gmail.com>
2020-04-27 12:05:27 -0700

    add merge sort solution

--- w16d1/merge_sort_project/lib/merge_sort.js
+++ w16d1/merge_sort_project/lib/merge_sort.js
@@ -1,8 +1,28 @@
 function merge(array1, array2) {
-
+    let result = [];
+    while (array1.length && array2.length) {
+        let ele1 = array1[0];
+        let ele2 = array2[0];
+        if (ele1 < ele2) {
+            result.push(array1.shift());
+        } else {
+            result.push(array2.shift())
+        }
+    }
+    return [...result, ...array1, ...array2]
 }
 
 function mergeSort(array) {
+    if(array.length <= 1) return array;
+
+    let midIdx = Math.floor(array.length/2);
+    let leftHalf = array.slice(0, midIdx)
+    let rightHalf = array.slice(midIdx)
+    let leftSorted = mergeSort(leftHalf);
+    let rightSorted = mergeSort(rightHalf);
+
+    return merge(leftSorted,rightSorted);
+
 
 }
 

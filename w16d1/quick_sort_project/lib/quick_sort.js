dbe48aecf66ae3e423468d94609a1fb8fca32f72
Manraj <manrajkhokhar1995@gmail.com>
2020-04-27 12:16:55 -0700

    add quick sort solution

--- w16d1/quick_sort_project/lib/quick_sort.js
+++ w16d1/quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,13 @@
 function quickSort(array) {
+    if(array.length <=1) return array;
 
+    let pivot = array.shift();
+    let leftArray = array.filter( el => el < pivot );
+    let rightArray = array.filter( el => el >= pivot );
+    let sortedLeft = quickSort(leftArray);
+    let sortedRight = quickSort(rightArray);
+
+    return sortedLeft.concat(pivot).concat(sortedRight)
 }
 
 

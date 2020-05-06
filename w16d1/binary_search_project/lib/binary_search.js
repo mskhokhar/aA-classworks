6f5484d258ef2bf03d85962801b7c5933fb3c595
Manraj <manrajkhokhar1995@gmail.com>
2020-05-06 14:40:28 -0700

    add binary search index solution

--- w16d1/binary_search_project/lib/binary_search.js
+++ w16d1/binary_search_project/lib/binary_search.js
@@ -14,8 +14,22 @@ function binarySearch(array, target) {
 }
 
 function binarySearchIndex(array, target) {
-
+    if(array.length < 1) return -1;
+    let mid = Math.floor(array.length/2);
+    let middleElement = array[mid];
+    if (middleElement === target) return mid;
+    let leftArray = array.slice(0, mid);
+    let rightArray = array.slice(mid + 1);
+    if (target < middleElement) {
+        return binarySearchIndex(leftArray, target);
+    } else {
+        let res = binarySearchIndex(rightArray, target);
+        return  res === -1 ? -1 : res + mid + 1  ;
+    }
 }
+
+let test = [3,4,5,6,7,22,25,65,45];
+binarySearchIndex(test,22);
 
 
 module.exports = {

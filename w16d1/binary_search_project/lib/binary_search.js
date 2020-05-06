4831b03407cfedc42bb406862bbe16bb9dea34a5
Manraj <manrajkhokhar1995@gmail.com>
2020-05-06 14:16:16 -0700

    Add solution of binary search without returning index

--- w16d1/binary_search_project/lib/binary_search.js
+++ w16d1/binary_search_project/lib/binary_search.js
@@ -1,5 +1,16 @@
 function binarySearch(array, target) {
-
+    if(array.length < 1) return false;
+    
+    let mid = Math.floor(array.length/2);
+    let middleElement = array[mid];
+    if (middleElement === target) return true;
+    let leftArray = array.slice(0,mid);
+    let rightArray = array.slice(mid+1);
+    if (target < middleElement) {
+        return binarySearch(leftArray, target);
+    } else {
+        return binarySearch(rightArray, target);
+    }
 }
 
 function binarySearchIndex(array, target) {

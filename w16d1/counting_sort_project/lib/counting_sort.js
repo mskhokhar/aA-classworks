cff2c1c5d619917373d6f0ecf19aa60d5fdfbe6a
Manraj <manrajkhokhar1995@gmail.com>
2020-05-06 14:05:45 -0700

    add counting sort solution

--- w16d1/counting_sort_project/lib/counting_sort.js
+++ w16d1/counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,18 @@
 function countingSort(arr, max) {
-
+    let result = [];
+    let counter = new Array(max + 1).fill(0);
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+        counter[element]++;
+    }
+    for (let i = 0; i < counter.length; i++) {
+        let element = counter[i];
+        while(element > 0){
+            result.push(i);
+            element--;
+        }
+    }
+    return result;
 }
 
 

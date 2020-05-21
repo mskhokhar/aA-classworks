340451d57b9102fce535d71db06140c580089dac
Manraj <manrajkhokhar1995@gmail.com>
2020-05-20 17:11:53 -0700

    Add stack solution

--- w16d1/stack_project/lib/stack.js
+++ w16d1/stack_project/lib/stack.js
@@ -21,11 +21,51 @@
 // -----------
 
 class Node {
-
+    constructor(newNode){
+        this.value = newNode;
+        this.next = null;
+    }
 }
 
 class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
 
+    push(input){
+        let newNode = new Node(input);
+        if (!this.top) {
+            this.top = newNode;
+            this.bottom = newNode;
+        }else{
+            let oldNode = this.top;
+            this.top = newNode;
+            this.top.next = oldNode;
+        }
+        this.length++;
+        return this.length;
+
+        
+    }
+    pop(){
+        let oldNode = this.top;
+        if (this.length === 0) {
+            return null;
+        }else if(this.length === 1){
+            this.top = this.bottom  = null;
+        }else{
+            this.top = oldNode.next;
+        }
+        this.length--;
+        return oldNode.value;
+        
+    }
+
+    size(){
+        return this.length;
+    }
 }
 
 exports.Node = Node;

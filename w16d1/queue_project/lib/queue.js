111dae3bfeba218a4acbfdd7e376dbad512e3fec
Manraj <manrajkhokhar1995@gmail.com>
2020-05-20 17:54:32 -0700

    add queue solution

--- w16d1/queue_project/lib/queue.js
+++ w16d1/queue_project/lib/queue.js
@@ -21,11 +21,45 @@
 // -----------
 
 class Node {
-
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Queue {
-
+    constructor() {
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
+    enqueue(val){
+        let newNode = new Node(val);
+        if(!this.front){
+            this.front = newNode;
+            this.back = newNode
+        }else{
+            this.back.next = newNode;
+            this.back = newNode;
+        }
+        return ++this.length;
+    }
+    dequeue(){
+        let oldNode = this.front;
+        if(!this.front){
+            return null
+        }else if(this.front === this.back){
+            this.front = this.back = null;
+        }
+        else{
+            this.front = oldNode.next;
+        }
+        this.length--;
+        return oldNode.value;
+    }
+    size(){
+        return this.length;
+    }
 }
 
 exports.Node = Node;

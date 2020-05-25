b0ac20c05fe800577cee28acfe14991971ceab80
Manraj <manrajkhokhar1995@gmail.com>
2020-05-25 15:34:39 -0700

    solution for balanced parenthesis

--- w16d2/stack_queue_interview_problems/lib/1_balanced_parens.js
+++ w16d2/stack_queue_interview_problems/lib/1_balanced_parens.js
@@ -76,7 +76,24 @@
 // Let's code!
 // -----------
 function balancedParens(str) {
+    let stack = [];
+    let brackets = {
+        '(' : ')',
+        '[' : ']',
+        '{' : '}'
+    }
+    for (let i = 0; i < str.length; i++) {
+        var char = str[i];
+        if (brackets[char]) {
+            stack.push(char);
+        } else if (char === ')' || char === ']' || char === '}'){
+            if (brackets[stack.pop()] !== char) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
 
-}
+}   
 
 exports.balancedParens = balancedParens;
